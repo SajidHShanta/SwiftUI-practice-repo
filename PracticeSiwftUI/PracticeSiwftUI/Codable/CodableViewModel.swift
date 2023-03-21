ec9bbd48d6459c12122b86eb6eaeba2f8e193db7
sajid <sajidhasan673@gmail.com>
2023-03-22 00:01:56 +0600

    manually decode data from JSON

--- PracticeSiwftUI/PracticeSiwftUI/Codable/CodableViewModel.swift
+++ PracticeSiwftUI/PracticeSiwftUI/Codable/CodableViewModel.swift
@@ -8,14 +8,29 @@
 import Foundation
 
 class CodableViewModel: ObservableObject {
-    @Published var customer: CustomerModel? = CustomerModel(id: "", name: "Sajid", point: 32, isPremium: false)
+    @Published var customer: CustomerModel? = nil
     
     init() {
         getData()
     }
     
     func getData() {
+        guard let data = getJSONData() else { return }
         
+//        let jsonString = String(data: data, encoding: .utf8)
+        
+        if
+            let localeData = try? JSONSerialization.jsonObject(with: data),
+            let dictionary = localeData as? [String: Any],
+            let id = dictionary["id"] as? String,
+            let name = dictionary["name"] as? String,
+            let point = dictionary["point"] as? Int,
+            let isPremium = dictionary["isPremium"] as? Bool {
+            
+            let newCustomer = CustomerModel(id: id, name: name, point: point, isPremium: isPremium)
+            customer = newCustomer
+            
+        }
     }
     
     func getJSONData() -> Data? {
