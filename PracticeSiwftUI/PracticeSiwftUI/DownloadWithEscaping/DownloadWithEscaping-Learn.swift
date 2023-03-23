81d984df7ade09a05cbd67ccccfc0cac5ab48261
sajid <sajidhasan673@gmail.com>
2023-03-23 23:30:20 +0600

    download JSON data from API

--- PracticeSiwftUI/PracticeSiwftUI/DownloadWithEscaping/DownloadWithEscaping-Learn.swift
+++ PracticeSiwftUI/PracticeSiwftUI/DownloadWithEscaping/DownloadWithEscaping-Learn.swift
@@ -9,11 +9,41 @@ import SwiftUI
 
 class DownloadWithEscapingViewModel: ObservableObject {
     init() {
-        
+        getData()
     }
     
     func getData() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1") else { return }
         
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data else {
+                print("No data")
+                return
+            }
+            
+            guard error == nil else {
+                print("Error: \(String(describing: error))")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                print("Invalid response")
+                return
+            }
+            
+            guard response.statusCode >= 200 && response.statusCode < 300 else {
+                print("Status code sholud be 2xx, but here \(response.statusCode)")
+                return
+            }
+            
+            print("Successfully downloaded data!")
+            print(data)
+            
+            let jsonString = String(data: data, encoding: .utf8)
+            print(jsonString ?? "no json data")
+            
+        }.resume()
     }
 }
 
